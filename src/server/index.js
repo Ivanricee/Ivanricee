3429deb5f544260623bfe64edb4c0e2cecc3a90a
Ivan Bautista <ivanbautistag@gmail.com>
2020-08-04 17:45:59 -0500

    mend
    deploy configuration

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,10 @@
 
 require('ignore-styles')
 
+//recibe una funcion con dos elements
+//nos permite hacer un bin en tiempo real de cualquier paquete que necesitemos
 require('@babel/register')({
+  ignore: [/(node_modules)/],
   presets: ['@babel/preset-env', '@babel/preset-react'],
 })
 
