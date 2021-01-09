2c005991604d3282e975f56ca7f90efae00e71a4
Ivan Bautista <ivanbautistag@gmail.com>
2021-01-08 18:10:11 -0600

    Intersection observer on reel section

--- src/frontend/components/Reel.jsx
+++ src/frontend/components/Reel.jsx
@@ -1,20 +1,42 @@
-import React /*, { useEffect }*/ from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { connect } from 'react-redux'
 
 const Reel = (props) => {
+  const refReelLoader = useRef(null)
+  const [loadReel, setLoadReel] = useState(false)
+  useEffect(() => {
+    const observerReel = new window.IntersectionObserver(function (entries) {
+      entries.forEach((entry) => {
+        const { isIntersecting } = entry
+        if (isIntersecting) {
+          setLoadReel(true)
+        }
+      })
+      return () => {
+        observerReel.disconnect()
+      }
+    })
+    observerReel.observe(refReelLoader.current)
+
+    return () => observerReel.disconnect()
+  }, [])
+
   return (
     <>
-      <div className="main__title">
+      <div className="main__title" ref={refReelLoader}>
         <h1>3D Reel.</h1>
       </div>
       <div className="main-reel__video-wrap">
-        <iframe
-          title="Texturing and look dev Reel"
-          src="https://www.youtube.com/embed/UOWbHDryB20?start=3"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
+        {loadReel === true && (
+          <iframe
+            title="Texturing and look dev Reel"
+            src="https://www.youtube.com/embed/UOWbHDryB20?start=3"
+            loading="lazy"
+            frameBorder="0"
+            allow="modestbranding=1; showinfo=0; controls=0; vq=hd720; autoplay; clipboard-write; encrypted-media; picture-in-picture"
+            allowFullScreen
+          />
+        )}
       </div>
     </>
   )
